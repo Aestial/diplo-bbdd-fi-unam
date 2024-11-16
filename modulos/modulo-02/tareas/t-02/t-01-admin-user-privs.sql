33420a3b74bafb5f0fa261277004abc5fa3124e2
Jaime Hernandez <jaimevazz@gmail.com>
2024-11-16 00:06:53 -0600

    New directory structure

---
+++ modulos/modulo-02/tareas/t-02/t-01-admin-user-privs.sql
@@ -0,0 +1,10 @@
+--@Autor: Hernandez Vazquez Jaime
+--@Fecha creación: 16/11/2024
+--@Descripción: Tarea 01. Administracion privilegios de usuarios.
+
+prompt 0. Generando spool
+spool jhv-t-02-admin-user-privs.txt
+
+
+
+
