fe852667d57e9b7394faada1115a48dae8b4eba8
Jaime Corona <jaimevazz@gmail.com>
2024-11-22 17:44:17 -0600

    M03 s-02

--- modulos/modulo-03/ejercicios/s-02-modos-inicio.sql
+++ modulos/modulo-03/ejercicios/s-02-modos-inicio.sql
@@ -1,10 +1,11 @@
 
+
 --@Autor: <Nombre del autor o autores>
 --@Fecha creación: <Fecha de creación>
 --@Descripción: <Breve descripción del contenido del script>
 
-prompt 1. Autenticando como sysdba en jrcdiplo_s2
-connect sys/system2@jrcdiplo_s2 as sysdba
+prompt 1. Autenticando como sysdba en gbldiplo_s2
+connect sys/system2@gbldiplo_s2 as sysdba
 
 prompt 2.1 Creando al usuario  user01
 drop user if exists user01 cascade;
@@ -20,10 +21,11 @@ grant create session, create table to user02;
 prompt 3. Abrir una terminal y entrar a sesión con el usuario user01
 pause Presionar [Enter] para continuar
 
-prompt 4. Iniciando en modo restringido en jrcdiplo_s2
+prompt 4. Iniciando en modo restringido en gbldiplo_s2
 prompt ¿Será posible pasar al modo restringido con el usuario USER01 en sesión?
 pause presionar [Enter] para confirmar la respuesta
 --#TODO
+--R: Sin problema, la BD podrá pasar al modo restrictivo.
 
 --TODO#
 
@@ -31,33 +33,37 @@ prompt 5. En la terminal del usuario USER01 intentar crear una tabla de prueba
 prompt e intentar crear un registro
 pause ¿Qué sucederá ? [Enter] para continuar
 --#TODO
-
+-- Abrir terminal, autenticar con user01, crear la tabla e insertar
+-- R: Sin problema, el usuario puede hacer cualquier acción porque ya estaba
+--conectado antes de cambiar al modo restringido
 --#TODO
 
-Prompt 6. Intentando crear sesión con user02 en jrcdiplo_s2
+Prompt 6. Intentando crear sesión con user02 en gbldiplo_s2
 pause [¿Qué sucederá?, Enter para continuar]
 
 --#TODO
-
+-- R: El usuario USER02 no podrá iniciar sesión porque ya está el modo restringido
+connect user02/user02@gbldiplo_s2
 --TODO#
 
-prompt 7. Autenticando como sysdba en jrcdiplo_s2
-connect sys/system2@jrcdiplo_s2 as sysdba
+prompt 7. Autenticando como sysdba en gbldiplo_s2
+connect sys/system2@gbldiplo_s2 as sysdba
 
 Prompt asignando el privilegio restricted session a user02
 --#TODO
-
+grant restricted session to user02;
 --TODO#
 
 Prompt 8. Intentando crear sesión con user02
 pause [¿Qué sucederá ?, Enter para continuar]
 
 --#TODO
+--R: El usuario podrá autenticar
 
 --#TODO
 
-Prompt 9. Regresando al modo no restringido en jrcdiplo_s2
-connect sys/system2@jrcdiplo_s2 as sysdba
+Prompt 9. Regresando al modo no restringido en gbldiplo_s2
+connect sys/system2@gbldiplo_s2 as sysdba
 --#TODO
 
 --TODO#
@@ -75,29 +81,33 @@ shutdown immediate
 
 pause 11. Conectando como user02 ¿Qué sucederá? [Enter] para continuar
 --#TODO
+-- R: Error, un usuario común no podrá autenticar
 
 --TODO#
 
 pause 12. Intentando autenticar como sysdba ¿Qué sucederá? [Enter] para continuar
 --#TODO
-
+--R:  Sin problema. Un usuario administrador puede autenticar en modo read only
+connect sys/system2@gbldiplo_s2 as sysdba
 --TODO#
 
 pause 13. Intentando autenticar como sysoper ¿Qué sucederá? [Enter] para continuar
 --#TODO
-
+--R:  Sin problema. Un usuario administrador puede autenticar en modo read only
+connect  sys/system2@gbldiplo_s2 as sysoper
 --TODO#
 
 pause 14. Intentando crear una tabla en el esquema public ¿Qué sucederá? [Enter] para continuar
 --#TODO
-
+--R: La tabla no podrá ser creada
+create table test01(id number);
 --TODO#
 
 Prompt 15. Regresar al modo de escritura y lectura
 connect sys/system2 as sysdba
 shutdown immediate 
 --#TODO
-
+startup open read write;
 --TODO#
 
 Prompt Listo
