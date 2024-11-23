2408263d69b638900fa787a8c71fa7b69e76a6cb
Jaime Corona <jaimevazz@gmail.com>
2024-11-22 21:41:59 -0600

    M0s-04 from codeshare

--- modulos/modulo-03/ejercicios/s-04-componentes-sga.sql
+++ modulos/modulo-03/ejercicios/s-04-componentes-sga.sql
@@ -14,6 +14,60 @@ Pause Analizar la consulta, [Enter] para continuar
 Prompt 3. Realizando consulta a v$sgainfo
 
 --#TODO
+select q1.pool, q1.num_componentes,
+       trunc(q1.megas_usados,2) megas_usados,
+       trunc(q2.megas_libres,2) megas_libres,
+       trunc(q1.megas_usados + nvl(q2.megas_libres,0),2) mb_asignados
+       from (
+        select pool, count(*) as num_componentes,  sum(bytes)/1024/1024 megas_usados
+        from v$sgastat
+        where name <> 'free memory'
+        and pool is not null
+        group by pool
+       ) q1
+       left outer join (
+        select pool, trunc(bytes/1024/1024,2) megas_libres
+        from v$sgastat
+        where name = 'free memory'
+        and pool is not null
+       ) q2
+       on q1.pool = q2.pool
+union all
+select name, 0, bytes/1024/1024, -1 ,bytes/1024/1024
+from v$sgastat
+where pool is null
+order by megas_usados desc;
+--TODO#
+
+
+--TODO#
+Pause Analizar la consulta, [Enter] para continuar
+
+Prompt 3. Realizando consulta a v$sgainfo
+select name, trunc(bytes/1024/1024) mb_asignados
+from v$sgainfo
+where name not in (
+        'Free SGA Memory Available',
+        'Maximum SGA Size',
+        'Granule Size',
+        'Startup overhead in Shared Pool'
+)
+union all
+select 'Memoria total', trunc(sum(bytes)/1024/1024,2) mb_asignados
+from v$sgainfo
+where name not in (
+        'Free SGA Memory Available',
+        'Maximum SGA Size',
+        'Granule Size',
+        'Startup overhead in Shared Pool'
+)
+group by 'Memoria total'
+union all
+select name, trunc(bytes/1024/1024,2)
+from v$sgainfo
+where name = 'Free SGA Memory Available'
+order by 2 desc;
+--#TODO
 
 --TODO#
 Pause Analizar la consulta, [Enter] para continuar
