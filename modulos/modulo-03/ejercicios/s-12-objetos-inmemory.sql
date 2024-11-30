9c24eea990ae70c18e73439418b46ba1a16b9010
Jaime Hernandez <jaimevazz@gmail.com>
2024-11-29 19:58:01 -0600

    M03 05 In memory area s-12

--- modulos/modulo-03/ejercicios/s-12-objetos-inmemory.sql
+++ modulos/modulo-03/ejercicios/s-12-objetos-inmemory.sql
@@ -13,11 +13,16 @@ set linesize window
 col title format a50
 
 --#TODO
-
+explain plan 
+set statement_id = 'q1' for
+select /*+ gather_plan_statistics */ title, trunc(duration/60, 1) duration_hrs
+from user03imc.movie where upper(title) like '%WAR%';
 --TODO#
 
 Prompt 3. Visualizando el plan de ejecución sin IM Column store.
 --#TODO
+select * from table(dbms_xplan.display(statement_id=> 'q1'));
+pause Analizar plan de ejecución [Enter] para continuar.
 
 --TODO#
 
@@ -27,26 +32,38 @@ alter table user03imc.movie inmemory;
 Prompt 5. Mostrando configuraciones asociadas con IM column store 
 col table_name format a20 
 --#TODO
-
+select table_name, inmemory, inmemory_compression, inmemory_priority,
+inmemory_distribute
+from dba_tables where table_name = 'MOVIE'
+and owner = 'USER03IMC';
 --TODO#
 pause Analizar resultados [Enter] para continuar
 
 Prompt 6. Realizando consulta en v$im_segments previa al acceso
 col segment_name format a20 
 --#TODO
-
+select segment_name, bytes/1024/1024 MBs, inmemory_size/1024/1024 inmemory_size_mb,
+  populate_status, inmemory_priority
+from v$im_segments;
+where segment_name = 'MOVIE'
+and owner = 'USER03IMC';
 --TODO#
 pause Analizar resultados [Enter] para continuar
 
 Prompt 7. Realizando una consulta para provocar el poblado de la IM Column store
 --#TODO
-
+select title, trunc(duration/60,1) duration_hrs
+from user03imc.movie where upper(title) like '%WAR%';
 --TODO#
 pause ¿Cuántos registros se obtuvieron? [Enter] para continuar
 
 Prompt 8. consultando nuevamente en v$im_segments
 --#TODO
-
+select segment_name, bytes/1024/1024 MBs, inmemory_size/1024/1024 inmemory_size_mb,
+  populate_status, inmemory_priority
+from v$im_segments;
+where segment_name = 'MOVIE'
+and owner = 'USER03IMC';
 --TODO#
 pause Analizar resultados [Enter] para continuar
 
@@ -56,37 +73,75 @@ col column_name format a20
 col minimum_value format a20
 col maximum_value format a50
 --#TODO
-
+select column_number, column_name, minimum_value, maximum_value
+from v$im_col_cu cu, dba_objects o, dba_tab_cols c
+where cu.objd = o.data_object_id
+and o.object_name = c.table_name
+and cu.column_number = c.column_id
+and o.owner = 'USER03IMC'
+and o.object_name = 'MOVIE';
+order by column_number, minimum_value;
 --TODO#
 pause Analizar resultados [Enter] para continuar
 
 Prompt 10. Deshabilitando el uso de la IM C Store para mostrar estadísticas
 --#TODO
-
+alter session set  inmemory_query=disable;
+select title, trunc(duration/60, 1) duration_hrs
+from user03imc.movie where upper(title) like '%WAR%';
 --TODO#
 
 Prompt 11. Mostrando estadísticas  del uso de la IM C Store  y sus IMCUs.
 col display_name format a30
 --#TODO
-
+select display_name,value
+from v$mystat m, v$statname n
+where m.statistic# = n.statistic#
+and display_name in (
+  'IM scan segments minmax eligible',
+  'IM scan CUs delta pruned',
+  'IM scan segments disk',
+  'IM scan bytes in-memory',
+  'IM scan bytes uncompressed',
+  'IM scan rows',
+  'IM scan blocks cache'
+);
 --TODO#
 
 pause Analizar resultados [Enter] para continuar
 
 Prompt 12. Habilitar nuevamente el uso de la IM C Store para mostrar estadísticas
 --#TODO
-
+alter session set  inmemory_query=enable;
+select title, trunc(duration/60, 1) duration_hrs
+from user03imc.movie where upper(title) like '%WAR%';
 --TODO#
-
 
 Prompt 13. Mostrando estadísticas  del uso de la IM C Store  y sus IMCUs.
 --#TODO
-
+select display_name,value
+from v$mystat m, v$statname n
+where m.statistic# = n.statistic#
+and display_name in (
+  'IM scan segments minmax eligible',
+  'IM scan CUs delta pruned',
+  'IM scan segments disk',
+  'IM scan bytes in-memory',
+  'IM scan bytes uncompressed',
+  'IM scan rows',
+  'IM scan blocks cache'
+);
 --TODO#
 Pause Analizar resultados, [Enter] para continuar
 
 Prompt 14. Mostrando nuevamente plan de ejecución con IM COlumn habilitada
 --#TODO
+explain plan 
+set statement_id = 'q2' for
+select /*+ gather_plan_statistics */ title, trunc(duration/60, 1) duration_hrs
+from user03imc.movie where upper(title) like '%WAR%';
+
+select * from table(dbms_xplan.display(statement_id=> 'q2'));
 
 --TODO#
 
@@ -94,7 +149,7 @@ pause Analizar resultados [Enter] para continuar
 
 Prompt 15. Deshabilitando el uso de la IM column para que este script sea idempotente
 --#TODO
-
+alter table user03imc.movie no inmemory;
 --TODO#
 
 --la limpieza del usuario se realiza en el script s-10-column-store.sql
