029694e6111f263d69bb0d2fd7b1284b72e914b0
Jaime Hernandez <jaimevazz@gmail.com>
2024-11-30 14:53:11 -0500

    Complete M03 s-14

--- modulos/modulo-03/ejercicios/s-14-monitor-mem.sql
+++ modulos/modulo-03/ejercicios/s-14-monitor-mem.sql
@@ -45,14 +45,113 @@ create table user04monitor.memory_areas(
 prompt Agregando un nuevo registro en user04monitor.memory_areas 
 --#TODO
 insert into user04monitor.memory_areas(
-  fecha,total_sga_1,total_sga_2,total_sga_3,sga_free,
-  pga_param,pga_total_2,pga_reservada,pga_reservada_max,pga_en_uso,pga_libre,pga_auto_w_areas,pga_manual_w_areas,
+  fecha,
+  --sga
+  total_sga_1,total_sga_2,total_sga_3, sga_free,
+  --pga
+  pga_param,pga_total_2,pga_reservada,pga_reservada_max,pga_en_uso,pga_libre,
+  pga_auto_w_areas,pga_manual_w_areas,
+  --pools
   log_buffer,db_buffer_cache,shared_pool,large_pool,java_pool,stream_pool,
-  inmemory
+  inmemory 
 ) values (
-  trunc(sysdate),
+  sysdate,
+  -- a partir de v$sga  
+  trunc(
+    (  
+      (select sum(value) from v$sga)-
+      (select current_size from v$sga_dynamic_free_memory)
+    )/1024/1024,2
+  )
+  ,
+  --a partir de v$sga_dynamic_components  
+  trunc(
+    (
+      (select sum(current_size) from v$sga_dynamic_components) +
+      (select value from v$sga where name ='Fixed Size') +
+      (select value from v$sga where name ='Redo Buffers')
+    ) /1024/1024,2  
+  ),
+  -- a partir de v$sgainfo  
+  trunc(
+    (
+      select sum(bytes) from v$sgainfo where name not in (
+      'Granule Size',
+      'Maximum SGA Size',
+      'Startup overhead in Shared Pool',
+      'Free SGA Memory Available',
+      'Shared IO Pool Size'
+      )
+    ) /1024/1024,2
+  ),
+  --Memoria libre
+  trunc(
+    (select current_size/1024/1024 from v$sga_dynamic_free_memory),2
+  ),
+  --total memoria PGA
+  trunc(
+    (select value/1024/1024 
+      from v$pgastat where name ='aggregate PGA target parameter'
+    ),2
+  ),
+  --total a partir de la RAM libre de la SGA
+  trunc(
+    (select current_size/1024/1024 from v$sga_dynamic_free_memory)  ,2
+  ),
+  --pga total reservada
+  trunc(
+    (select value/1024/1024 
+      from v$pgastat where name ='total PGA allocated'
+    ),2
+  ),
+  --pga total reservada máxima
+  trunc(
+    (select value/1024/1024 
+      from v$pgastat where name ='maximum PGA allocated'
+    ),2
+  ),
+  --pga en uso 
+  trunc(
+    (select value/1024/1024 
+      from v$pgastat where name ='total PGA inuse'
+    ),2
+  ),
+  --pga libre
+  trunc(
+    (select value/1024/1024 
+      from v$pgastat where name ='total freeable PGA memory'
+    ),2
+  ),
+  --pga work areas auto
+  trunc(
+    (select value/1024/1024 
+      from v$pgastat where name ='total PGA used for auto workareas'
+    ),2
+  ),
+  --pga work areas manual
+  trunc(
+    (select value/1024/1024 
+      from v$pgastat where name ='total PGA used for manual workareas'
+    ),2
+  ),
+  --log buffer
+  (select trunc(bytes/1024/1024,2) from v$sgainfo where name='Redo Buffers'),
+  --db buffeer cache
+  (select trunc(bytes/1024/1024,2) from v$sgainfo where name='Buffer Cache Size'),
+  --shared pool
+  (select trunc(bytes/1024/1024,2) from v$sgainfo where name='Shared Pool Size'),
+  --large pool
+  (select trunc(bytes/1024/1024,2) from v$sgainfo where name='Large Pool Size'),  
+  --Java pool size
+  (select trunc(bytes/1024/1024,2) from v$sgainfo where name='Java Pool Size'),
+  --stream pool size 
+  (select trunc(bytes/1024/1024,2) from v$sgainfo where name='Streams Pool Size'),
+  --IM size
+  (select trunc(bytes/1024/1024,2) from v$sgainfo where name='In-Memory Area Size')
   
-)
+  
+  
+);
 --TODO#
 
 commit;
