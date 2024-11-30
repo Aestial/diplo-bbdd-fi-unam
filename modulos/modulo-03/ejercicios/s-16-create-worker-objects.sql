894ab3616575c120169d61cc00feaf7869be78de
Jaime Hernandez <jaimevazz@gmail.com>
2024-11-30 14:56:47 -0500

    Complete M03 s-16

--- modulos/modulo-03/ejercicios/s-16-create-worker-objects.sql
+++ modulos/modulo-03/ejercicios/s-16-create-worker-objects.sql
@@ -50,6 +50,48 @@ show errors
 
 --procedimiento empleado para analizar resultados
 --#TODO
-create or replace procedure 
+create or replace procedure sp_process_data is  
+  v_c1_pattern number(1,0);
+  v_c2_pattern varchar2(1);
+  v_c3_pattern varchar2(1);    
+  v_c1_count number;
+  v_c2_count number;
+  v_c3_count number;
+  cursor cur_data is
+    select id,c1_number,c2_alpha,c3_printable_char
+    from random_data;
+  v_query varchar2(1000) := '
+    insert into data_results(
+      c1_pattern,c1_count,
+      c2_pattern,c2_count,
+      c3_pattern,c3_count)
+    values(:ph1,:ph2,:ph3,:ph4,:ph5,:ph6)
+  ';
+begin
+  v_c1_pattern:=trunc(dbms_random.value(0,10));
+  v_c2_pattern:=dbms_random.string('A',1);faffff
+  v_c3_pattern:=dbms_random.string('P',1);  
+  for r in cur_data loop
+    v_c1_count := regexp_count(r.c1_number,v_c1_pattern);
+    v_c2_count := regexp_count(r.c2_alpha,v_c2_pattern);
+    v_c3_count := regexp_count(r.c3_printable_char,v_c3_pattern);
+    execute immediate v_query using 
+      v_c1_pattern,v_c1_count,
+      v_c2_pattern,v_c2_count,
+      v_c3_pattern,v_c3_count;
+  end loop;
+
+  --insertando resultados finales
+  insert into total_results
+    select c1_pattern, sum(c1_count), count(*) from data_results
+    group by c1_pattern
+    union 
+    select c2_pattern, sum(c2_count), count(*) from data_results
+    group by c2_pattern
+    union
+    select c3_pattern, sum(c3_count), count(*) from data_results
+    group by c3_pattern;
+end;
+/
 --TODO#
 show errors
