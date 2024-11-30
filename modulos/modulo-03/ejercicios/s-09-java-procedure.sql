a01acd7f513eec47da0e3160df922ecb7453f72f
Jaime Hernandez <jaimevazz@gmail.com>
2024-11-29 18:00:08 -0600

    M03 05 in memory area s-10

--- modulos/modulo-03/ejercicios/s-09-java-procedure.sql
+++ modulos/modulo-03/ejercicios/s-09-java-procedure.sql
@@ -8,30 +8,41 @@ connect c##userJava/userJava
 
 Prompt 2. Creando procedimiento almacenado
 --#TODO
-
+create or replace procedure resizeImage(
+  filePath varchar2, width number, height number) as language java
+  name 'mx.edu.unam.fi.dipbd.ResizeImage.resizeImage(java.lang.String,int,int)';
+/
 --TODO#
 
 prompt 3. Copiando la imagen a /tmp
 !cp paisaje.png /tmp 
 
-prompt 4. Invocando el procedimiento
+prompt 4. Invocando el procedimiento  --# Num 3 en documento.md
 --#TODO
-
+exec resizeImage('/tmp/paisaje.png',734,283)
 --TODO#
 
-prompt 5. Mostrando el contenido de la carpeta /tmp
+prompt 5. Mostrando el contenido de la carpeta /tmp --# Num 4 en documento.md
 !ls -lh /tmp/paisaje.png
 !ls -lh /tmp/output-paisaje.png
 
 Prompt 6. Mostrando operaciones de ajuste de memoria para el java pool
-
+--# Num 5 en documento.md
 connect sys/system2 as sysdba 
 col component format a15
 col parameter format a15
 set linesize window
 alter session set nls_date_format='dd/mm/yyyy hh24:mi:ss';
 
---#TODO
-
+--#TODO Query
+select component, oper_type, 
+  trunc(initial_size/1024/1024) initial_size_mb,
+  trunc(target_size/1024/1024) target_size_mb,
+  trunc(final_size/1024/1024) final_size_mb,
+  start_time, parameter
+from v$sga_resize_ops
+where component = 'java pool'
+order by start_time;
 --TODO#
+
 pause [Enter] para continuar
