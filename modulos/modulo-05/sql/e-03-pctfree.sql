4f1b1d02bd624fa075fc5ce34eed5f495e5befce
Jaime Hernandez <jaimevazz@gmail.com>
2024-12-13 20:35:09 -0600

    M05 02.md e-03-pctfree

---
+++ modulos/modulo-05/sql/e-03-pctfree.sql
@@ -0,0 +1,51 @@
+--@Autor:  Jaime Hernandez Vazquez
+--@Fecha creación:  13/12/2024
+--@Descripción: pctfree ejemplos
+
+whenever sqlerror exit rollback
+connect jaime05/jaime
+
+Prompt creando tabla con pctfree 0
+drop table if exists jaime05.t02_random_str_0;
+
+create table t02_random_str_0(
+  str char(18) 
+) pctfree 0;
+
+Prompt creando tabla con pctfree 50
+create table t02_random_str_50(
+  str char(18) 
+) pctfree 50;
+
+Pause Iniciando carga de 10,000 registros por tabla [Enter] para iniciar.
+declare
+  v_str char(18);
+begin
+  v_str := rpad('A',18,'X');
+  for v_index in 1..10000 loop
+    insert into t02_random_str_0 values(v_str);
+    insert into t02_random_str_50 values(v_str);
+  end loop;
+end;
+/
+commit;
+set pagesize 100
+Prompt consultando total de bloques a través del rowid para pctfree 0
+select dbms_rowid.rowid_relative_fno(rowid) as file_number,
+  dbms_rowid.rowid_block_number(rowid) as block_number,
+  count(*) blocks_in_file
+from t02_random_str_0
+group by dbms_rowid.rowid_relative_fno(rowid),
+  dbms_rowid.rowid_block_number(rowid)
+order by 1,2;
+
+Prompt consultando total de bloques a través del rowid para pctfree 50
+select dbms_rowid.rowid_relative_fno(rowid) as file_number,
+  dbms_rowid.rowid_block_number(rowid) as block_number,
+  count(*) blocks_in_file
+from t02_random_str_50
+group by dbms_rowid.rowid_relative_fno(rowid),
+  dbms_rowid.rowid_block_number(rowid)
+order by 1,2;
+
+
